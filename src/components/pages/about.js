dde520985f587cd3807228061465958f970864da
GabrielG01 <gabrielprovo01@gmail.com>
2019-10-31 11:28:14 -0600

    Added about page and working on contact page

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
+import picture from "../../../static/assets/images/auth/login.jpg";
 
 const About = () => {
-    return (
-        <div className=''>
-            About
-        </div>
-    )
-}
+  return (
+    <div className="content-page-wrapper">
+      <div
+        className="left-column"
+        style={{
+          background: "url(" + picture + ") no-repeat",
+          backgroundSize: "cover",
+          backgroundPosition: "center"
+        }}
+      />
+      <div className="right-column">
+        Super epic bio here. I'm 35, epic and super cringe. I love bad babes and
+        booze!
+      </div>
+    </div>
+  );
+};
 
-export default About    
+export default About;
